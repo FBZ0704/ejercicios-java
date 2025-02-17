9dd607dba28fea5a9add144cceec1cd371c507d8
FBZ0704 <fbuitragos@hotmail.com>
2025-02-17 12:03:11 -0500

    Update EstadoAgua.java

--- EstadoAgua.java
+++ EstadoAgua.java
@@ -5,5 +5,20 @@ import java.util.Scanner;
 // Pedir al usuario el valor de la temperatura. 
 
 public class EstadoAgua {
-    
+    public static void main(String[] args) {
+        Scanner Scanner = new Scanner(System.in);
+        System.out.println("Por Favor Indique A Que Temperatura Esta El Agua?");
+        
+        int temperatura = Scanner.nextInt();
+        if (temperatura <= 0) {
+          System.out.println("El Agua Esta A: " + temperatura + " Grados Centigrados, Por Lo Tanto El Agua Esta En Estado Solido");
+        }else if (temperatura > 0 && temperatura < 100) {
+          System.out.println("El Agua Esta A: " + temperatura + "Grados Centigrados, Por Lo Tanto El Agua Esta En Estado Liquido");
+        }if (temperatura >= 100) {
+          System.out.println("El Agua Esta En Estado Gaseoso");
+        }
+
+        Scanner.nextInt();
+        Scanner.close();
+      }
 }
