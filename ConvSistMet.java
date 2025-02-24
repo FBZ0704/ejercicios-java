5bdc6240317a0e101a041c4837953c998cbfe569
FBZ0704 <fbuitragos@hotmail.com>
2025-02-24 17:37:58 -0500

    Update ConvSistMet.java

--- ConvSistMet.java
+++ ConvSistMet.java
@@ -4,34 +4,39 @@ import java.util.Scanner;
    Hacer un programa que realice la conversión de metros a pies y de pies a metros.
 */
 
-public class ConvSistMet {
+public class Conversion {
   public static void main(String[] args) {
+    Scanner Scanner = new Scanner(System.in);
     // Variables
     double pies;
     Double metros;
     Double mts;
 
-      Scanner Scanner = new Scanner(System.in);
+    
+    int dato = 0;
+
+    while (dato == 0) {
       System.out.println(" Escribe: \n1 - Para Convertir Pies a Metros \n2 - Para Convertir Metros A Pies \n0 - Para Terminar");
       int opcion = Scanner.nextInt();
 
-      if (opcion > 0) {
-        if (opcion == 1) {
-          Scanner Scanner2 = new Scanner(System.in);
-          System.out.println(" Digita La Cantidad De Pies: ");
-          pies = Scanner2.nextDouble();
-          metros = (pies / 3.28);
-          System.out.println("El Resultado Es: " + metros + " mts");
-          Scanner2.close();
-        } else if (opcion == 2) {
-          Scanner Scanner2 = new Scanner(System.in);
-          System.out.println(" Digita La Cantidad De Metros: ");
-          mts = Scanner2.nextDouble();
-          pies = mts * 3.28;
-          System.out.println("El Resultado Es: " + pies + " pies");
-          Scanner2.close();
-        }
-        Scanner.close();
+      if (opcion == 1) {
+        Scanner Scanner2 = new Scanner(System.in);
+        System.out.println(" Digita La Cantidad De Pies: ");
+        pies = Scanner2.nextDouble();
+        metros = (pies / 3.28);
+        System.out.println("El Resultado Es: " + metros + " mts");
+      }else if (opcion == 2) {
+         Scanner Scanner2 = new Scanner(System.in);
+         System.out.println(" Digita La Cantidad De Metros: ");
+         mts = Scanner2.nextDouble();
+         pies = mts * 3.28;
+         System.out.println("El Resultado Es: " + pies + " pies");
+       } else if (opcion == 0){
+         dato = 1;
+      }
+
+      Scanner.close();
+      Scanner2.close();
       }
   }
 }
